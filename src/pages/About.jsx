8fd01804e7f6e599f78084f71a016aa2f7e4c7ea
lyalex011 <lytvolex@gmail.com>
2023-08-03 13:02:38 -0600

    about page bug fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,19 +20,19 @@ function About() {
               <div className="mb-6 md:mb-0">
               <h2 className="mb-6 text-3xl font-bold">Frequently asked questions</h2>
                 <p className="mb-4 font-bold">Are these images free to use?</p>
-                <p className="mb-12 text-neutral-500 dark:text-neutral-300">
-                All images on this website are from <a className="text-blue-600 dark:text-blue-500 hover:underline" href="https://unsplash.com/license">Unsplash.com</a>. Their licence grants you an irrevocable, nonexclusive, worldwide copyright license to download, copy, modify, distribute, perform, and use photos from Unsplash for free, including for commercial purposes, without permission from or attributing the photographer or Unsplash. This license does not include the right to compile photos from Unsplash to replicate a similar or competing service.
+                <p className="mb-12 text-neutral-500 ">
+                All images on this website are from <a className="text-blue-600  hover:underline" href="https://unsplash.com/license">Unsplash.com</a>. Their licence grants you an irrevocable, nonexclusive, worldwide copyright license to download, copy, modify, distribute, perform, and use photos from Unsplash for free, including for commercial purposes, without permission from or attributing the photographer or Unsplash. This license does not include the right to compile photos from Unsplash to replicate a similar or competing service.
                 </p>
         
                 <p className="mb-4 font-bold">What the difference beetwen "Copy link" and "Download" option?</p>
-                <p className="mb-12 text-neutral-500 dark:text-neutral-300">
+                <p className="mb-12 text-neutral-500 ">
                 QuickPic offers two convenient options: "Download" image to save your chosen image in JPG or WebP format, and "Copy Link" to seamlessly embed the image on your live web projects. Empower your creativity with our stylish images and elevate your projects effortlessly!
                 </p>
         
                 <p className="mb-4 font-bold">
                   Why webp format doesn't work with blur settings?
                 </p>
-                <p className="mb-12 text-neutral-500 dark:text-neutral-300">
+                <p className="mb-12 text-neutral-500 ">
                     With "Download" option we recommend you to save your chosen image in JPG format, as the WebP format doesn't support blur settings due to API limitations.
                 </p>
               </div>
